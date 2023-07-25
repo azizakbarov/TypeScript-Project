06720617c4e4a9f52e8360ba1e6aef69c4a532db
Aziz Akbarov <azizbakbarov@gmail.com>
2023-07-25 12:39:26 +0500

    Rendering HTML template

--- public/app.js
+++ public/app.js
@@ -1,39 +1,8 @@
 import { Invoice } from './classes/invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
-//interfaces
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-// docOne = new Invoice ('yoshi', 'web work', 250 );
-// docTwo = new Payment ('mario', 'plumbing work', 350);
-// interface isPerson {
-//     name: string;
-//     age: number;
-//     speak(a: string): void;
-//     spend(a: number): number; 
-// }
-// const me: isPerson = {
-//     name:'Alex',
-//     age: 30,
-//     speak(text: string): void {
-//         console.log(text);
-//     },
-//     spend (amount: number): number {
-//         console.log('I spent', amount);
-//         return amount;
-//     }
-// }
-//     const greetPerson = (person: isPerson) => {
-//         console.log('Hello there', person.name)
-//     }
-//     greetPerson(me);
-// const invOne = new Invoice ('mario', 'for the website', 250);
-// const invTwo = new Invoice ('luigi', 'for the website', 300);
-// let invoices: Invoice[] = []; 
-// invoices.push(invOne);
-// invoices.push(invTwo);
-// invoices.forEach( inv => {
-//     console.log(inv.client, inv.amount, inv.format())
-// }); 
+const ul = document.querySelector('ul');
+const list = new ListTemplate(ul);
 const form = document.querySelector('.new-item-form');
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -48,5 +17,5 @@ form.addEventListener('submit', (e) => {
     else {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 });
